c301286cf92e19f5cf6de0ed5ca552b49a8d672f
Paolo Zanotti <paolo.zanotti@reactbricks.com>
2023-03-13 15:25:33 +0100

    Fix Header & Footer Layout

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext } from "react"
 import {
   ReactBricksContext,
   PageViewer,
@@ -6,24 +6,28 @@ import {
   fetchPages,
   cleanPage,
   types,
-} from 'react-bricks/frontend'
-import Head from 'next/head'
-import { GetStaticProps, GetStaticPaths } from 'next'
+} from "react-bricks/frontend"
+import Head from "next/head"
+import { GetStaticProps, GetStaticPaths } from "next"
 
-import config from '../react-bricks/config'
-import Layout from '../components/layout'
-import ErrorNoPage from '../components/errorNoPage'
+import config from "../react-bricks/config"
+import Layout from "../components/layout"
+import ErrorNoPage from "../components/errorNoPage"
 
 interface PageProps {
   page: types.Page
+  header: types.Page
+  footer: types.Page
   error: string
 }
 
-const Page: React.FC<PageProps> = ({ page, error }) => {
+const Page: React.FC<PageProps> = ({ page, header, footer, error }) => {
   // Clean the received content
   // Removes unknown or not allowed bricks
   const { pageTypes, bricks } = useContext(ReactBricksContext)
   const pageOk = page ? cleanPage(page, pageTypes, bricks) : null
+  const headerOk = header ? cleanPage(header, pageTypes, bricks) : null
+  const footerOk = footer ? cleanPage(footer, pageTypes, bricks) : null
 
   return (
     <Layout>
@@ -31,26 +35,30 @@ const Page: React.FC<PageProps> = ({ page, error }) => {
         <>
           <Head>
             <title>{page.meta.title}</title>
-            <meta name="description" content={page.meta.description} />
+            <meta name='description' content={page.meta.description} />
           </Head>
+          <PageViewer page={headerOk} />
           <PageViewer page={pageOk} />
+          <PageViewer page={footerOk} />
         </>
       )}
-      {error === 'NOPAGE' && <ErrorNoPage />}
+      {error === "NOPAGE" && <ErrorNoPage />}
     </Layout>
   )
 }
 
 export const getStaticProps: GetStaticProps = async (context) => {
   if (!config.apiKey) {
-    return { props: { error: 'NOKEYS' } }
+    return { props: { error: "NOKEYS" } }
   }
   const { slug } = context.params
   try {
     const page = await fetchPage(slug.toString(), config.apiKey, context.locale)
-    return { props: { page } }
+    const header = await fetchPage("header", config.apiKey, context.locale)
+    const footer = await fetchPage("footer", config.apiKey, context.locale)
+    return { props: { page, header, footer } }
   } catch {
-    return { props: { error: 'NOPAGE' } }
+    return { props: { error: "NOPAGE" } }
   }
 }
 
