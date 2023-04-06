9d617bbbfb5b48017e55f8eaa3d977e1b415126c
Cecilia Panicali <cecilia@f2.net>
2023-04-06 10:21:55 +0200

    Updated to react-bricks v3.9.0-beta.4. Added fontFamily and screens to tailwind.config.js.

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,14 @@ module.exports = {
   darkMode: "class",
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Nunito Sans', ...defaultTheme.fontFamily.sans],
+      },
+      screens: {
+        sm: '600px',
+        lg: '900px',
+        xl: '1200px',
+      },
       boxShadow: {
         newsLetter:
           "0 1px 3px 0 rgba(0, 0, 0, 0.1) , 0 5px 15px 0 rgba(0, 0, 0, 0.05)",
