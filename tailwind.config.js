b4a53d91f7cbdd3534d9d1e7bd5209834ffcac52
FrancescoMottaDev <francesco.motta@reactbricks.com>
2023-04-07 14:11:42 +0200

    Tailwind config : delete color import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
-const defaultTheme = require("tailwindcss/defaultTheme")
-const colors = require("tailwindcss/colors")
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
-    "./pages/**/*.tsx",
-    "./components/**/*.tsx",
-    "./react-bricks/**/*.{ts,tsx}",
+    './pages/**/*.tsx',
+    './components/**/*.tsx',
+    './react-bricks/**/*.{ts,tsx}',
   ],
-  darkMode: "class",
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
@@ -20,9 +19,9 @@ module.exports = {
       },
       boxShadow: {
         newsLetter:
-          "0 1px 3px 0 rgba(0, 0, 0, 0.1) , 0 5px 15px 0 rgba(0, 0, 0, 0.05)",
+          '0 1px 3px 0 rgba(0, 0, 0, 0.1) , 0 5px 15px 0 rgba(0, 0, 0, 0.05)',
       },
     },
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [require('@tailwindcss/forms')],
 }
