92a811b7b4266249ccef82bab9ce42b48f763574
Matteo Frana <f2@f2.net>
2023-04-12 14:11:52 +0200

    TagListItem: changed tags styles in light/dark mode

--- components/TagListItem.tsx
+++ components/TagListItem.tsx
@@ -9,7 +9,7 @@ const TagListItem: React.FC<TagListItemProps> = ({ tag }) => {
   return (
     <Link
       href={`/blog/tag/${tag}`}
-      className="inline-block text-sm mr-2 mb-2 transform transition-all duration-200 text-gray-800 dark:text-gray-100 border border-gray-100 dark:border-gray-800 bg-gray-100 dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-900 hover:text-sky-600 dark:hover:text-sky-400 hover:border-sky-500 dark:hover:border-sky-500 hover:-translate-y-0.5 rounded-md py-1.5 px-2.5"
+      className="inline-block text-sm mr-2 mb-2 transform transition-all duration-200 text-sky-900 dark:text-gray-100 bg-sky-100 dark:bg-white/20 hover:bg-sky-200 dark:hover:hover:bg-sky-500/40  dark:hover:text-white hover:-translate-y-0.5 rounded-md py-1.5 px-2.5"
     >
       {tag}
     </Link>
