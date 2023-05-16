49b4c576744be4cd530ca5a7ca0ba462ffcfab45
Matteo Frana <f2@f2.net>
2023-05-16 19:56:01 +0200

    Theme name: 'Custom'

--- react-bricks/bricks/index.ts
+++ react-bricks/bricks/index.ts
@@ -5,7 +5,7 @@ import HeroUnit from './custom/MyHeroUnit'
 const bricks: types.Theme[] = [
   reactBricksUITheme, // React Bricks UI
   {
-    themeName: 'Custom Theme 1',
+    themeName: 'Custom',
     categories: [
       {
         categoryName: 'Hero sections',
