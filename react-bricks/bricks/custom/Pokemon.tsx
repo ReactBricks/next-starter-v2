285ae7d1d051c103abfe4a9f8547f1c018c2da51
Paolo Zanotti <paolo.zanotti@reactbricks.com>
2023-10-24 16:01:38 +0200

    ✏️ Remove section and container from pokemon brick

--- react-bricks/bricks/custom/Pokemon.tsx
+++ react-bricks/bricks/custom/Pokemon.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import { types } from 'react-bricks/frontend'
-import Section from '../react-bricks-ui/shared/components/Section'
-import Container from '../react-bricks-ui/shared/components/Container'
 
 interface PokemonProps {
   id: number
@@ -19,20 +17,22 @@ const Pokemon: types.Brick<PokemonProps> = ({
   imageUrl,
 }) => {
   if (!id || !name || !height || !weight || !imageUrl) {
-    return <div className="text-center text-red-500 underline text-xl">Pokemon not found!</div>
+    return (
+      <div className="text-center text-red-500 underline text-xl">
+        Pokemon not found!
+      </div>
+    )
   }
   return (
-    <Section>
-      <Container>
-        <img src={imageUrl} className="mx-auto w-36 mb-4" />
+    <div className="container max-w-3xl mx-auto">
+      <img src={imageUrl} className="mx-auto w-36 mb-4" />
 
-        <h1 className="text-5xl font-extrabold text-center mb-6">{name}</h1>
+      <h1 className="text-5xl font-extrabold text-center mb-6">{name}</h1>
 
-        <p className="text-center">
-          #{id} - Height {height / 10} m - Weight {weight / 10} Kg
-        </p>
-      </Container>
-    </Section>
+      <p className="text-center">
+        #{id} - Height {height / 10} m - Weight {weight / 10} Kg
+      </p>
+    </div>
   )
 }
 
